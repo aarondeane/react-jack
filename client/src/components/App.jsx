d25b9722d80d795debd5ac7ef1ad917479799507
aarondeane <aaronmdeane@gmail.com>
2019-09-13 15:00:21 -0700

    Initial boilerplate and setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return ( 
+      <div className="container">Welcome To React BlackJack!</div>
+    )
+  }
+}
+
+export default App;
